82211df0c69ff6c69565891782e9b5dcb4ce9c58
Adrian <adrian.pinon2329@gmail.com>
2020-11-07 18:57:00 -0600

    about to start all over

--- weatherscript.js
+++ weatherscript.js
@@ -1,8 +1,12 @@
 function weathDash() {
-    let inputEl = document.getElementById("city-input");
+    let inputEl = document.getElementById("city");
     let searchEl = document.getElementById("search-button");
     let nameEl = document.getElementById("city-name");
    
-}   
-weathDash();
+    let APIkey = "0b5b1a641aebdb629546fd9176f0f37f";
 
+    function getWeather(cityName) {
+    
+         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+}   
+weathDash();}
