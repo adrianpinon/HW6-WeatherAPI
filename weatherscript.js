c78f3017d68ac7c2f57ed7168f6092271ebd6c7b
Adrian <adrian.pinon2329@gmail.com>
2020-11-08 21:13:45 -0600

    added some function for js

--- weatherscript.js
+++ weatherscript.js
@@ -20,3 +20,25 @@ for (let i = 0; i < localStorage.length; i++) {
     let cityName = $(".list-group")
     cityName.append(`<ul>${city}</ul>`);
 }
+let keyCount = 0;
+searchButton.click(function () {
+
+    let searchInput = $(".searchInput").val();
+    
+    let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    if (searchInput == "") {
+        console.log(searchInput);
+    } else {
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function (response) {
+            // list-group append an li to it with just set text
+            // console.log(response.name);
+            let cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<ul>" + response.name + "</ul>");
+            // Local storage
+            let local = localStorage.setItem(keyCount, response.name);
+            keyCount = keyCount + 1;
+
+
