9472ed68233e2aff2c838441e57dcd002926507b
Adrian <adrian.pinon2329@gmail.com>
2020-11-08 18:25:14 -0600

    cleaned up HTML head and not even sure what I am doing with the JS but I have a plan

--- weatherscript.js
+++ weatherscript.js
@@ -1,11 +1,13 @@
 function weathDash() {
-    let inputEl = document.getElementById("city");
-    let searchEl = document.getElementById("search-button");
-    let nameEl = document.getElementById("city-name");
+    //let inputEl = document.getElementById("city");
+    //let searchEl = document.getElementById("search-button");
+    //let nameEl = document.getElementById("city-name");
    
+    
+    let searchButton = $(".searchButton")
     let APIkey = "0b5b1a641aebdb629546fd9176f0f37f";
 
-    function getWeather(cityName) {
+    //function getWeather(cityName) {
     
          let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 }   
